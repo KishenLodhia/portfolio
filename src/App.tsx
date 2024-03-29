dac2b0c192697c9a8e406b3beca9065d4b5b868a
Kishen Lodhia <kishan1999lodhia@gmail.com>
2024-03-29 12:46:24 +1000

    initial configuration: vite + react + tailwind CSS + ShadCN/UI + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { ModeToggle } from "./components/ui/mode-toggle";
+
+function App() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <ModeToggle />
+    </div>
+  );
+}
+
+export default App;
