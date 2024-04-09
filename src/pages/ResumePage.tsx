08beb1bccc9a51046d9c67cf67935c28433a3ffa
Kishen Lodhia <kishan1999lodhia@gmail.com>
2024-04-10 09:20:15 +1000

    add education in reume page

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -219,32 +219,111 @@ const ExperienceSection = [
   {
     company: "Solutions Studio",
     role: "Developer/Consultant",
-    start: "April 2023",
-    end: "Present",
+    start: "December 2022",
+    end: "July 2023",
     details: [
       {
-        title: "Eviole web application",
-        content: `Engineered a platform merging a custom web application with and
-    existing WordPress functionalities, enabling easy and dynamic content. Collaborated with marketing to refine the design.`,
+        title: "Real-estate CRM Solution",
+        content: `Designed and implemented a custom backend
+        solution for a realty company, this led to enhancing the customer relationships through a modern web application. The
+        solution helped the client by reducing their day-to-day activities by 85%.`,
       },
       {
-        title: "API development",
-        content: `Developed 2 APIs following the OpenAPI 3.0 spec. Firstly, to communicate with a leading
-        Australian Electronic Medical Record (eMR) System. Secondly, JWT authenticated API to communicate with the eviole
-        mobile app.`,
+        title: "Mobile Application - Tonga 'Atautolu",
+        content: `Developed a personal diary feature for the Tongan Government
+        Covid-19 contact tracing app. It allowed users to keep track of their location during the pandemic.`,
       },
       {
-        title: "Automated testing",
-        content: `Carried out automated end to end tests for the whole web application`,
-      },
-      {
-        title: "Mobile app",
-        content: `Created a mobile app with UI/UX design considerations, preserving all functionalities and enhancing
-        user engagement.`,
+        title: "Mobile App - Healing Plants of Fiji",
+        content: `Re-engineered the app’s architecture by using the provider and consumer design
+        pattern, this resulted in an increase of the app’s framerate by 20% and reduced the API calls by 25%. I also assisted with
+        training and improving the plant’s detection algorithm by 4%.`,
       },
       {
         title: "Stack",
-        content: `PHP, JavaScript, Flutter, REST APIs, Cypress, HTML, CSS`,
+        content: `Laravel MVC, MySQL, Eloquent ORM, Kotlin, SQLite, Flutter`,
+      },
+    ],
+  },
+
+  {
+    company: "The University of the South Pacific",
+    role: "Lead Software Engineer/Project Manager",
+    start: "July 2022",
+    end: "December 2022",
+    details: [
+      {
+        title: "University Mobile App",
+        content: `Led a team of software developers and collaborated with stakeholders in an
+        agile environment (SRUM). Designed and implemented the mobile app such that it can be used by 20,000 students per
+        semester. Additionally, we focused on student security, this feature utilised geo-location and integration with campus
+        security to allow for real-time communication between students and campus security.`,
+      },
+      {
+        title: "Web Portal and API",
+        content: `Developed a web application to manage and modify the
+        mobile app's content (keeping it dynamic). This included implementing an API with JWT authentication to keep
+        communication secure and encrypting the data while transmitting over the network.`,
+      },
+      {
+        title: "Stack",
+        content: `Flutter, REST API, Laravel, JWT, Bootstrap`,
+      },
+    ],
+  },
+];
+
+const EducationSection = [
+  {
+    uniName: "Queensland University of Technology",
+    shortName: "QUT",
+    start: "July 2022",
+    end: "July 2024",
+    details: [
+      {
+        title: "Course",
+        content: `Master of Information Technology (Computer Science)`,
+      },
+      {
+        title: "GPA",
+        content: `6.33/7`,
+      },
+    ],
+    awards: [
+      {
+        title: "Scholarship Award",
+        content: `International Merit Scholarship`,
+      },
+      {
+        title: "Academic Award",
+        content: `Deans Excellence Award`,
+      },
+    ],
+  },
+
+  {
+    uniName: "The University of the South Pacific",
+    shortName: "USP",
+    start: "January 2018",
+    end: "December 2021",
+    details: [
+      {
+        title: "Course",
+        content: `Bachelor of Software Engineering`,
+      },
+      {
+        title: "GPA",
+        content: `4.1/4.5`,
+      },
+    ],
+    awards: [
+      {
+        title: "Gold Medal",
+        content: `Most outstanding Software Engineering graduate`,
+      },
+      {
+        title: "Academic Award",
+        content: `Outstanding Discipline Award for Computer Science`,
       },
     ],
   },
@@ -294,6 +373,38 @@ const ResumePage = () => {
 
               {experienceItem.details.map((detail) => (
                 <ul className="p-2 list-disc ml-2 md:ml-5" key={detail.content}>
+                  <li>
+                    <p className="font-semibold text-lg text-primary">
+                      {detail.title}:{" "}
+                      <span className="text-primary opacity-90 font-normal text-base ">{detail.content}</span>
+                    </p>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          ))}
+        </ResumeContentDiv>
+
+        <ResumeContentDiv heading="Education">
+          {EducationSection.map((item) => (
+            <div key={item.start}>
+              <h2 className="text-2xl pt-3 text-slate-500 font-semibold">
+                {item.uniName} ({item.shortName})
+              </h2>
+              <ContentHeading text={`${item.start} - ${item.end}`} />
+              {item.details.map((detail) => (
+                <ul className="p-2 list-disc ml-2 md:ml-5" key={detail.content}>
+                  <li>
+                    <p className="font-semibold text-lg text-primary">
+                      {detail.title}:{" "}
+                      <span className="text-primary opacity-90 font-normal text-base ">{detail.content}</span>
+                    </p>
+                  </li>
+                </ul>
+              ))}
+              <ContentHeading text="Awards" />
+              {item.awards.map((detail) => (
+                <ul className="p-2 list-disc ml-2 md:ml-5" key={detail.title}>
                   <li>
                     <p className="font-semibold text-lg text-primary">
                       {detail.title}:{" "}
